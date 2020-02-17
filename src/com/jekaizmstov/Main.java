3f9cf698eadb01f823d7ac5664165883629fdbde
Tkachenko Evgenij <jekaizmostov@gmail.com>
2020-02-17 20:45:11 +0200

    Fourth commit/ add all clothe and fix some ui

--- src/com/jekaizmstov/Main.java
+++ src/com/jekaizmstov/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
+
 public class Main extends Application {
 
     @Override
@@ -21,6 +23,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
